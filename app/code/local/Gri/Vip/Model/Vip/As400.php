7b75475dcf0fb3ef5a55198279e407bb38258c88
gri-jack <gri-jack@users.noreply.github.com>
2014-08-30 01:00:19 +0800

    As400 function modification, add DEV PORT and mapping country

--- app/code/local/Gri/Vip/Model/Vip/As400.php
+++ app/code/local/Gri/Vip/Model/Vip/As400.php
@@ -3,89 +3,130 @@
 
 class Gri_Vip_Model_Vip_As400 extends Mage_Core_Model_Abstract
 {
-	const API_DOMAIN = 'http://as400.griretail.com';
-	const API_PORT = '62051';
-	
-	private $customerGroup;
+    const API_DOMAIN = 'http://as400.griretail.com';
+    const API_PORT = '62051';
+    const API_DEV_PORT = '62151';
 
-	public function _construct()
-	{
-		$this->_init('gri_vip/vip_as400');
-	}
+    private $customerGroup;
 
-	public function getCustomerGroup()
-	{
-		if(!$this->customerGroup)
-		{
-			$data = array();
-			$groups = Mage::getSingleton('customer/group')->getCollection();
-			foreach($groups as $group)
-			{
-				$data[strtolower($group->getCode())] = $group->getId();
-			}
+    public function _construct()
+    {
+        $this->_init('gri_vip/vip_as400');
+    }
 
-			$this->customerGroup = $data;
-			unset($data);
-			unset($groups);
-		}	
+    public function getCustomerGroup()
+    {
+        if(!$this->customerGroup)
+        {
+            $data = array();
+            $groups = Mage::getSingleton('customer/group')->getCollection();
+            foreach($groups as $group)
+            {
+                $data[strtolower($group->getCode())] = $group->getId();
+            }
 
-		return $this->customerGroup;
-	}
+            $this->customerGroup = $data;
+            unset($data);
+            unset($groups);
+        }
 
-	public function offlineGradeMapping($level)
-	{
-		$mapping = array('light grey'=>'grey','light_grey'=>'grey','black'=>'platinum');
-	
-		return isset($mapping[strtolower($level)])?$mapping[strtolower($level)]:$level;
-	}
+        return $this->customerGroup;
+    }
 
-	private function getAPIUrl($method="api")
-	{
-		return self::API_DOMAIN.':'.self::API_PORT.'/'.$method;
-	}
+    public function offlineGradeMapping($level)
+    {
+        $mapping = array('light grey'=>'grey','light_grey'=>'grey','black'=>'platinum');
 
-	public function checkVipAccount($data=array())
-	{
-		$method = 'VipDetail';
-		$json = $this->createJson($method, $data);
-		$ret =	$this->callWebService($this->getAPIUrl('api'),array('data'=>$json));
+        return isset($mapping[strtolower($level)])?$mapping[strtolower($level)]:$level;
+    }
 
-		return $ret;
-	}
+    private function getAPIUrl($method="api", $isDev=0)
+    {
+        return self::API_DOMAIN.':'.($isDev?self::API_DEV_PORT:self::API_PORT).'/'.$method;
+    }
 
-	public function checkVipContent($vipPk = "")
-	{
-		$method = 'VipList';
-		$json = $this->createJson($method, array('search1'=>$vipPk));
-		$ret = $this->callWebService($this->getAPIUrl('api'), array('data'=>$json));
-		
-		return $ret;
-	}
+    public function checkVipAccount($data=array())
+    {
+        $method = 'VipDetail';
+        $json = $this->createJson($method, $data);
+        $ret =	$this->callWebService($this->getAPIUrl('api'),array('data'=>$json));
 
-	public function checkVipInfo($vipPk = "")
-	{
-		$method = 'VipInfo';
-		$json = $this->createJson($method, array('pk#'=>$vipPk));
-		$ret = $this->callWebService($this->getAPIUrl('api'), array('data'=>$json));
-		return $ret;
-	}
+        return $ret;
+    }
 
-	private function createJson($method="",$data=array())
-	{
-		$output = array();
-		$output['method'] = $method;
-		$output['data'] = $data;
-		return json_encode($output);
-	}
+    public function checkVipContent($vipPk = "")
+    {
+        $method = 'VipList';
+        $json = $this->createJson($method, array('search1'=>$vipPk));
+        $ret = $this->callWebService($this->getAPIUrl('api'), array('data'=>$json));
 
-	private function callWebService($url, $data=array())
-	{
-		$c = curl_init($url);
-		curl_setopt($c, CURLOPT_POST,1);
-		curl_setopt($c, CURLOPT_POSTFIELDS,'data='.$data['data']);
-		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-		$tmp = curl_exec($c);
-		curl_close($c);
-		return json_decode($tmp, true);
-	}
+        return $ret;
+    }
+
+    public function checkVipInfo($vipPk = "")
+    {
+        $method = 'VipInfo';
+        $json = $this->createJson($method, array('pk#'=>$vipPk));
+        $ret = $this->callWebService($this->getAPIUrl('api'), array('data'=>$json));
+        return $ret;
+    }
+
+    private function createJson($method="",$data=array())
+    {
+        $output = array();
+        $output['method'] = $method;
+        $output['data'] = $data;
+        return json_encode($output);
+    }
+
+    private function callWebService($url, $data=array())
+    {
+        $c = curl_init($url);
+        curl_setopt($c, CURLOPT_POST,1);
+        curl_setopt($c, CURLOPT_POSTFIELDS,'data='.$data['data']);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        $tmp = curl_exec($c);
+        curl_close($c);
+        return json_decode($tmp, true);
+    }
+
+    private function getAs400CountryMapping()
+    {
+        return array('HK'=>'HKG','MY'=>'MAL','SG'=>'SIN','TW'=>'TWN','TH'=>'THA','MO'=>'MAC','CN'=>'CHN','JP'=>'JPN');
+    }
+
+    public function createAddArray(Mage_Customer_model_Customer $customer)
+    {
+        $countryMapping = $this->getAs400CountryMapping();
+        $data = array();
+
+        $email = $customer->getEmail();
+        $firstname = $customer->getFirstname();
+        $surname = $customer->getLastname();
+        $salulation = strtoupper($customer->getTitle());
+        $grade = 'GREY';
+        $vip_country = 'HKG';
+        $dob = $customer->getDob();
+        $mobile = $customer->getMobile();
+        $cardno = "0";
+        $district = $customer->getCustomerAreaCode();
+        $country = isset($countryMapping[$customer->getCustomerCountryId()])?$countryMapping[$customer->getCustomerCountryId()]:'HKG';
+        $join_date = date("Ymd");
+        $amend_user = $amend_date = $amend_time = "";
+        $currentVipPoint = ".00";
+        $age_group = "0";
+        $gender = ($salulation == 'MR'?'M':'F');
+        $address = $customer->getMailingAddress();
+
+        $data = compact('vip_country','expiry_date','cardno','grade','salulation','firstname','surname','gender','mobile','country','age_group','birthday','other_phone','email','address','join_date','amend_user','amend_date','amend_time','address');
+
+        $data['pk#'] = '0';
+        $data['current vip point'] = $currentVipPoint;
+        $data['district#'] = $district;
+        $data['birthday'] = '(DD)'.substr($dob,8,2).'(MM)'.substr($dob,5,2).'(YYYY)0';
+
+        return $data;
+    }
+
 }
+
